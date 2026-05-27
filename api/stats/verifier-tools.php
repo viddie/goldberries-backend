9a73fb9a22a103918473ff3c2407b6373be516f3
Hannes Ru <viddie@hotmail.de>
2026-05-27 18:22:25 +0200

    Add undecided sugg. count to verifier tools

--- api/stats/verifier-tools.php
+++ api/stats/verifier-tools.php
@@ -8,23 +8,16 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
 }
 
 $account = get_user_data();
-if ($account === null) {
-  die_json(401, "Not logged in");
-} else if (!is_helper($account)) {
-  die_json(403, "Forbidden");
-}
+check_role($account, $HELPER);
 
 $query = "
 SELECT
   (SELECT COUNT(*) FROM submission WHERE is_verified IS NULL) AS submissions_in_queue,
   (SELECT COUNT(*) FROM account WHERE claimed_player_id IS NOT NULL) AS open_player_claims,
-  (SELECT COUNT(*) FROM suggestion WHERE is_verified IS NULL) AS pending_suggestions
+  (SELECT COUNT(*) FROM suggestion WHERE is_verified IS NULL) AS pending_suggestions,
+  (SELECT COUNT(*) FROM suggestion WHERE is_verified = TRUE AND is_accepted IS NULL AND date_created < NOW() - INTERVAL '" . Suggestion::$expiration_days . " days') AS undecided_suggestions
 ";
-$result = pg_query($DB, $query);
-if (!$result) {
-  die_json(500, "Could not query database");
-}
-
+$result = pg_query_params_or_die($DB, $query);
 $row = pg_fetch_assoc($result);
-api_write($row);
+api_write($row, true);
 #endregion
